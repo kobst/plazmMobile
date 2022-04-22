4186e7fef702d23ee05be090d1cf3b40701d4d58
Edward Han <edwrdhn@gmail.com>
2022-04-22 10:40:11 -0400

    Initial Commit

---
+++ Shared/plazmMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  plazmMobileApp.swift
+//  Shared
+//
+//  Created by Edward Han on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct plazmMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
